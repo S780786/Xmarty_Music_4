b79c9d483ffa27a8d1eba60dc54923d2a12c718b
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2021-09-18 21:15:35 +0530

    Update reply.py

--- userplugins/reply.py
+++ userplugins/reply.py
@@ -1,6 +1,6 @@
 """
 XMARTY MUSIC PLAYER, Telegram Voice Chat Userbot
-Copyright (C) 2021  ZauteKm <https://telegram.dog/ZauteKm>
+Copyright (C) 2021  Legend Salim <https://t.me/Xmarty_Support>
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
