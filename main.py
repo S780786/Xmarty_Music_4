9a6b7a513b3dcd5edb445741cad85160cd011d8e
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2021-09-18 19:27:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
-VC Music Player, Telegram Voice Chat Userbot
-Copyright (C) 2021  Zaute Km
+XMARTY MUSIC PLAYER, Telegram Voice Chat Userbot
+Copyright (C) 2021  legend Salim
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
