dbcc091ee7c61d6cd970d2a88b0f7bbb67a5f84a
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2021-09-18 22:59:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/LushaiMusic/VCMusicPlayer.git /VCMusicPlayer
-cd /VCMusicPlayer
+git clone https://github.com/S780821/XMARTY_MUSIC_4.git /Xmarty_music_4
+cd /XMARTY_MUSIC_4
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
