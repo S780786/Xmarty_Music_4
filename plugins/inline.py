dceed8f19d77eec3b748fd254c159e1f5b52d23d
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2021-09-18 22:51:33 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -1,6 +1,6 @@
 """
-VC Music Player, Telegram Voice Chat Userbot
-Copyright (C) 2021  Zaute Km | TGVCSETS
+VC MusXmarty Player, Telegram Voice Chat Userbot
+Copyright (C) 2021  Legend Salim
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
